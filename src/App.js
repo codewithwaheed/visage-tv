fe5944bc0e382f338d5f27f618dc19fb2f09bf15
Waheed Ahmad <57209119+codewithwaheed@users.noreply.github.com>
2021-07-19 18:02:27 +0500

    commit' ' '

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from "./Components/Header/Header";
 import { Route } from "react-router-dom";
 import LandingPage from "./pages/landingPage/LandingPage";
 import ContactUs from "./pages/contactUs/ContactUs";
-
 import Sidebar from "./Components/Sidebar/sideBar";
 import ContextWrapper from "./context/ContextWrapper";
 export default function App() {
